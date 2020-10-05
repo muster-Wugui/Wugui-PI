19021a5b655c9672da6de28af0d89a6a49fd8b15
muster-Wugui <67823650+muster-Wugui@users.noreply.github.com>
2020-10-05 21:19:59 +0800

    Demo
    This program is used to demonstrate the function I defined

---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include "tinpi.h"
+#include <iostream>
+#include <wiringPi.h>
+
+/* Demonstrate the ultrasonic ranging function
+int main(){
+	int dist = UdmMod_hcsr04();
+	printf("%d \n",dist);
+	return 0;
+}
+*/
