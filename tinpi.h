0d06c257aa8ff5f03ab2726869087a299d81b435
muster-Wugui <67823650+muster-Wugui@users.noreply.github.com>
2020-10-07 11:02:12 +0800

    Head File
    I declare my function in this program

--- tinpi.h
+++ tinpi.h
@@ -1,4 +1,7 @@
 #ifndef _TINPI_H
 #define _TINPI_H
-int UdmMod_hcsr04(void);//Declare ultrasonic ranging function, this function is applied to hcsr04
+int UdmMod_hcsr04(void);
+int PriMod_hcsr501(void);
+void BuzzMod_Passivebuzzer(int freq,int t_ms);
+int SwMod_Powerswitch(void);
 #endif
